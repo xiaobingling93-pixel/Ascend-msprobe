8c880dd71af99468523ae4a2f443fa151d130213
mnhdxnh <yangchen160@huawei.com>
2025-11-29 17:33:58 +0800

    【review】添加ut
    Co-authored-by: mnhdxnh<yangchen160@huawei.com>

---
+++ test/msprobe_test/pytorch_ut/api_accuracy_checker/run_ut/test_run_ut.py
@@ -0,0 +1,288 @@
+# coding=utf-8
+import os
+import copy
+import shutil
+import tempfile
+import unittest
+from unittest.mock import patch, DEFAULT
+from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check import *
+from msprobe.core.common.file_utils import get_json_contents, create_directory, save_json, write_csv
+from msprobe.core.common.exceptions import FileCheckException
+from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check_utils import UtDataInfo, exec_api
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+forward_file = os.path.join(base_dir, "forward.json")
+forward_content = get_json_contents(forward_file)
+for api_full_name, api_info_dict in forward_content.items():
+    api_full_name = api_full_name
+    api_info_dict = api_info_dict
+
+
+class Args:
+    def __init__(self, config_path=None, api_info_path=None, out_path=None, result_csv_path=None):
+        self.config_path = config_path
+        self.api_info_path = api_info_path
+        self.out_path = out_path
+        self.result_csv_path = result_csv_path
+
+
+class TestFileCheck(unittest.TestCase):
+    def setUp(self):
+        src_path = 'temp_path'
+        create_directory(src_path)
+        dst_path = 'soft_link'
+        os.symlink(src_path, dst_path)
+        self.hard_path = os.path.abspath(src_path)
+        self.soft_path = os.path.abspath(dst_path)
+        json_path = os.path.join(self.hard_path, 'test.json')
+        json_data = {'key': 'value'}
+        save_json(json_path, json_data)
+        self.hard_json_path = json_path
+        soft_json_path = 'soft.json'
+        os.symlink(json_path, soft_json_path)
+        self.soft_json_path = os.path.abspath(soft_json_path)
+        csv_path = os.path.join(self.hard_path, 'test.csv')
+        csv_data = [['1', '2', '3']]
+        write_csv(csv_data, csv_path)
+        soft_csv_path = 'soft.csv'
+        os.symlink(csv_path, soft_csv_path)
+        self.csv_path = os.path.abspath(soft_csv_path)
+        self.empty_path = "empty_path"
+
+    def tearDown(self):
+        os.unlink(self.soft_json_path)
+        os.unlink(self.csv_path)
+        os.unlink(self.soft_path)
+        for file in os.listdir(self.hard_path):
+            os.remove(os.path.join(self.hard_path, file))
+        os.rmdir(self.hard_path)
+
+    def test_config_path_soft_link_check(self):
+        args = Args(config_path=self.soft_json_path, api_info_path=self.hard_json_path, out_path=self.hard_path)
+
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.SOFT_LINK_ERROR)
+
+    def test_api_info_path_soft_link_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.soft_json_path, out_path=self.hard_path)
+
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.SOFT_LINK_ERROR)
+
+    def test_out_path_soft_link_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.hard_json_path, out_path=self.soft_path)
+
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.SOFT_LINK_ERROR)
+
+    def test_result_csv_path_soft_link_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.hard_json_path, out_path=self.hard_path,
+                    result_csv_path=self.csv_path)
+
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.SOFT_LINK_ERROR)
+
+    def test_config_path_empty_check(self):
+        args = Args(config_path=self.empty_path, api_info_path=self.hard_json_path, out_path=self.hard_path)
+
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_api_info_path_empty_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.empty_path, out_path=self.hard_path)
+
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_out_path_empty_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.hard_json_path, out_path=self.empty_path)
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_result_csv_path_empty_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.hard_json_path, out_path=self.hard_path,
+                    result_csv_path=self.empty_path)
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_config_path_invalid_check(self):
+        args = Args(config_path=123, api_info_path=self.hard_json_path, out_path=self.hard_path)
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_api_info_path_invalid_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path="123", out_path=self.hard_path)
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_out_path_invalid_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.hard_json_path, out_path=123)
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+    def test_result_csv_path_invalid_check(self):
+        args = Args(config_path=self.hard_json_path, api_info_path=self.hard_json_path, out_path=self.hard_path,
+                    result_csv_path=123)
+        with self.assertRaises(Exception) as context:
+            acc_check_command(args)
+            self.assertEqual(context.exception.code, FileCheckException.ILLEGAL_PATH_ERROR)
+
+
+class TestRunUtMethods(unittest.TestCase):
+    def test_exec_api(self):
+        api_info = copy.deepcopy(api_info_dict)
+
+        [api_type, api_name, _, _] = api_full_name.split(".")
+        args, kwargs, need_grad = get_api_info(api_info, api_name, None)
+        cpu_params = generate_cpu_params(args, kwargs, True, '')
+        cpu_args, cpu_kwargs = cpu_params.cpu_args, cpu_params.cpu_kwargs
+        cpu_exec_params = ExecParams(api_type, api_name, Const.CPU_LOWERCASE, cpu_args, cpu_kwargs, False, None)
+        out = exec_api(cpu_exec_params)
+        self.assertEqual(out[0].dtype, torch.float32)
+        self.assertTrue(out[0].requires_grad)
+        self.assertEqual(out[0].shape, torch.Size([2048, 2, 1, 128]))
+
+    def test_generate_device_params(self):
+        mock_tensor = torch.rand([2, 2560, 24, 24], dtype=torch.float32, requires_grad=True)
+
+        with patch.multiple('torch.Tensor',
+                            to=DEFAULT,
+                            clone=DEFAULT,
+                            detach=DEFAULT,
+                            requires_grad_=DEFAULT,
+                            type_as=DEFAULT,
+                            retain_grad=DEFAULT) as mocks:
+            mocks['clone'].return_value = mock_tensor
+            mocks['detach'].return_value = mock_tensor
+            mocks['requires_grad_'].return_value = mock_tensor
+            mocks['type_as'].return_value = mock_tensor
+            mocks['retain_grad'].return_value = None
+            mocks['to'].return_value = mock_tensor
+
+            device_args, device_kwargs, _ = generate_device_params([mock_tensor], {'inplace': False}, True, '')
+            self.assertEqual(len(device_args), 1)
+            self.assertEqual(device_args[0].dtype, torch.float32)
+            self.assertTrue(device_args[0].requires_grad)
+            self.assertEqual(device_args[0].shape, torch.Size([2, 2560, 24, 24]))
+            self.assertEqual(device_kwargs, {'inplace': False})
+
+    def test_generate_cpu_params(self):
+        api_info = copy.deepcopy(api_info_dict)
+        [api_type, api_name, _, _] = api_full_name.split(".")
+        args, kwargs, need_grad = get_api_info(api_info, api_name, None)
+        cpu_params = generate_cpu_params(args, kwargs, True, '')
+        cpu_args, cpu_kwargs = cpu_params.cpu_args, cpu_params.cpu_kwargs
+        self.assertEqual(len(cpu_args), 2)
+        self.assertEqual(cpu_args[0].dtype, torch.float32)
+        self.assertTrue(cpu_args[0].requires_grad)
+        self.assertEqual(cpu_args[0].shape, torch.Size([2048, 2, 1, 256]))
+        self.assertEqual(cpu_kwargs, {'dim': -1})
+
+    def test_UtDataInfo(self):
+        data_info = UtDataInfo(None, None, None, None, None, None, None)
+        self.assertIsNone(data_info.bench_grad)
+        self.assertIsNone(data_info.device_grad)
+        self.assertIsNone(data_info.device_output)
+        self.assertIsNone(data_info.bench_output)
+        self.assertIsNone(data_info.grad_in)
+        self.assertIsNone(data_info.in_fwd_data_list)
+
+    def test_blacklist_and_whitelist_filter(self):
+        api_name = "test_api"
+        black_list = ["test_api"]
+        white_list = []
+        result = blacklist_and_whitelist_filter(api_name, black_list, white_list)
+        self.assertTrue(result)
+
+        api_name = "test_api"
+        black_list = []
+        white_list = ["another_api"]
+        result = blacklist_and_whitelist_filter(api_name, black_list, white_list)
+        self.assertTrue(result)
+
+        api_name = "test_api"
+        black_list = ["test_api"]
+        white_list = ["test_api"]
+        result = blacklist_and_whitelist_filter(api_name, black_list, white_list)
+        self.assertTrue(result)
+
+        api_name = "test_api"
+        black_list = []
+        white_list = ["test_api"]
+        result = blacklist_and_whitelist_filter(api_name, black_list, white_list)
+        self.assertFalse(result)
+
+    def test_supported_api(self):
+        api_name = "torch.matmul"
+        result = is_unsupported_api(api_name)
+        self.assertFalse(result)
+
+        api_name = "Distributed.all_reduce"
+        result = is_unsupported_api(api_name)
+        self.assertTrue(result)
+
+    def test_no_backward(self):
+        grad_index = None
+        out = (1, 2, 3)
+        result = need_to_backward(grad_index, out)
+        self.assertFalse(result)
+
+        grad_index = 0
+        out = 42
+        result = need_to_backward(grad_index, out)
+        self.assertTrue(result)
+
+    def test_check_need_grad_given_out_kwarg_then_return_false(self):
+        from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check import check_need_grad
+
+        api_info_dict = {"input_kwargs": {"out": True}}
+        result = check_need_grad(api_info_dict)
+        self.assertFalse(result)
+
+    def test_check_need_grad_given_no_out_kwarg_then_return_true(self):
+        from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check import check_need_grad
+
+        api_info_dict = {"input_kwargs": {}}
+        result = check_need_grad(api_info_dict)
+        self.assertTrue(result)
+
+    def test_preprocess_forward_content_given_duplicate_apis_then_filter(self):
+        from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check import preprocess_forward_content
+
+        forward_content = {
+            "torch.add_1": {"input_args": [{"value": 1}], "input_kwargs": {}},
+            "torch.add_2": {"input_args": [{"value": 1}], "input_kwargs": {}},
+            "torch.sub": {"input_args": [{"value": 2}], "input_kwargs": {}}
+        }
+
+        result = preprocess_forward_content(forward_content)
+
+        self.assertEqual(len(result), 2)  # One duplicate should be removed
+
+    def test_initialize_save_error_data_given_valid_path_then_return_path(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.test_dir = self.temp_dir.name
+
+        from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check import initialize_save_error_data
+
+        error_data_path = os.path.join(self.test_dir, "error_data")
+        result = initialize_save_error_data(error_data_path)
+
+        self.assertTrue(os.path.exists(result))
+        self.assertIn("ut_error_data", result)
+        self.temp_dir.cleanup()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/msprobe_test/pytorch_ut/api_accuracy_checker/run_ut/test_run_ut_utils.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2024-2025, Huawei Technologies Co., Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import torch
+
+from msprobe.pytorch.api_accuracy_checker.acc_check.acc_check_utils import *
+from msprobe.core.common.file_utils import create_directory, write_csv
+
+
+class TestRunUtUtils(unittest.TestCase):
+    def setUp(self):
+        save_path = "temp_save_path"
+        create_directory(save_path)
+        self.save_path = os.path.realpath(save_path)
+        self.result_file_name = "accuracy_checking_result_12345678901234.csv"
+        self.detail_file_name = "accuracy_checking_details_12345678901234.csv"
+        self.invalid_file_name = "invalid_file_name.csv"
+        self.result_csv_path = os.path.join(self.save_path, self.result_file_name)
+        self.details_csv_path = os.path.join(self.save_path, self.detail_file_name)
+        self.invalid_csv_path = os.path.join(self.save_path, self.invalid_file_name)
+        content = [["api_name", "metric_name", "metric_value", "metric_unit"]]
+        write_csv(content, self.result_csv_path)
+        write_csv(content, self.details_csv_path)
+        write_csv(content, self.invalid_csv_path)
+        
+    def tearDown(self):
+        for filename in os.listdir(self.save_path):
+            os.remove(os.path.join(self.save_path, filename))
+        os.rmdir(self.save_path)
+
+    def test_get_validated_result_csv_patht_valid_mode(self):
+        validated_path = get_validated_result_csv_path(self.result_csv_path, 'result')
+        self.assertEqual(validated_path, self.result_csv_path)
+
+    def test_get_validated_result_csv_path_invalid_mode(self):
+        with self.assertRaises(ValueError):
+            get_validated_result_csv_path(self.result_csv_path, 'invalid_mode')
+
+    def test_get_validated_result_csv_path_file_path_validation(self):
+        validated_path = get_validated_result_csv_path(self.result_csv_path, 'result')
+        self.assertEqual(validated_path, self.result_csv_path)
+
+    def test_get_validated_result_csv_patht_result_csv_name_pattern(self):
+        validated_path = get_validated_result_csv_path(self.result_csv_path, 'result')
+        self.assertEqual(validated_path, self.result_csv_path)
+
+    def test_get_validated_result_csv_path_invalid_result_csv_name_pattern(self):
+        with self.assertRaises(ValueError):
+            get_validated_result_csv_path(self.invalid_csv_path, 'result')
+
+    def test_get_validated_details_csv_path_file_name_replacement(self):
+        validated_details_csv_path = get_validated_details_csv_path(self.result_csv_path)
+        self.assertEqual(validated_details_csv_path, self.details_csv_path)
+
+    def test_exec_api_functional_api(self):
+        api_name = "sigmoid"
+        args = (torch.tensor([1]))
+        kwargs={}
+        api_type = "Functional"
+        exec_params = ExecParams(api_type, api_name, "cpu", args, kwargs, False, None)
+        result = exec_api(exec_params)
+        self.assertTrue(torch.allclose(result, torch.tensor(0.7311), atol=1e-4))
+
+    def test_exec_api_tensor_api(self):
+        api_name = "add"
+        args = (torch.tensor(1), torch.tensor(2))
+        kwargs={}
+        api_type = "Tensor"
+        exec_params = ExecParams(api_type, api_name, "cpu", args, kwargs, False, None)
+        result = exec_api(exec_params)
+        self.assertEqual(result, torch.tensor(3))
+
+    def test_exec_api_torch_api(self):
+        api_name = "add"
+        args = (torch.tensor(1), torch.tensor(2))
+        kwargs={}
+        api_type = "Torch"
+        exec_params = ExecParams(api_type, api_name, "cpu", args, kwargs, False, None)
+        result = exec_api(exec_params)
+        self.assertEqual(result, torch.tensor(3))
+
+    def test_raise_bench_data_dtype_dtype_unchanged(self):
+        arg = torch.tensor(1.0, dtype=torch.float32)
+        raise_dtype = torch.float32
+        result = raise_bench_data_dtype("api_name", arg, raise_dtype)
+        self.assertEqual(result, arg)
+
+    def test_raise_bench_data_dtype_dtype_changed(self):
+        arg = torch.tensor(1.0, dtype=torch.float32)
+        raise_dtype = torch.float64
+        result = raise_bench_data_dtype("api_name", arg, raise_dtype)
+        self.assertEqual(result.dtype, raise_dtype)
+
+    def test_raise_bench_data_dtype_hf_32_standard_api(self):
+        arg = torch.tensor(1.0, dtype=torch.float32)
+        result = raise_bench_data_dtype("conv2d", arg)
+        self.assertEqual(result, arg)
